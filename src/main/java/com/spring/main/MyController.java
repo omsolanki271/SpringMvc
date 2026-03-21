c773c26ddcf04bf00393a245b9b2fefbdb00aad0
omsolanki271 <omsolanki271@gmail.com>
2026-03-21 11:40:43 +0530

    remove HttpServletRequest and pass data using @ReuestParam

--- src/main/java/com/spring/main/MyController.java
+++ src/main/java/com/spring/main/MyController.java
@@ -6,49 +6,62 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
 
 	/* first way */
-	//@RequestMapping(value = "/helloPage",method = RequestMethod.GET)
+	// @RequestMapping(value = "/helloPage",method = RequestMethod.GET)
 	/* second way */
 	@GetMapping("/helloPage")
-	public ModelAndView OpenHelloPage()
-	{
+	public ModelAndView OpenHelloPage() {
 		System.out.println("openHelloPage() method execute");
-		
+
 		ModelAndView mav = new ModelAndView();
-		//call to viewResolver with name  
+		// call to viewResolver with name
 		mav.setViewName("hello");
 		return mav;
 	}
-	
+
 	@GetMapping("/about")
-	public String openAboutpage()
-	{
+	public String openAboutpage() {
 		return "about";
 	}
-	
+
 	@GetMapping("/MyForm")
-	public String openmyform()
-	{
+	public String openmyform() {
 		return "my-form";
 	}
+
+	/* First Way to pass data (Not use in company- only learning purpose) */
+
+	/*
+	 * @PostMapping("/submitForm") public String handleMyform(HttpServletRequest
+	 * request) { String uname = request.getParameter("username"); String email =
+	 * request.getParameter("email"); String mobileno =
+	 * request.getParameter("mobile");
+	 * 
+	 * System.out.println(uname); System.out.println(email);
+	 * System.out.println(mobileno);
+	 * 
+	 * return "profile"; }
+	 */
+
+	/* Second Way to pass data */
+
 	@PostMapping("/submitForm")
-	public String handlMyform(HttpServletRequest request)
+	public String handleMyform(@RequestParam("username") String uname,
+							 @RequestParam("email") String email,
+							 @RequestParam("mobile") String mobileno) 
 	{
-		String uname = request.getParameter("username");
-		String email = request.getParameter("email");
-		String mobileno = request.getParameter("mobile");
-		
 		System.out.println(uname);
 		System.out.println(email);
 		System.out.println(mobileno);
-		
 		return "profile";
 	}
 }
